6dfa8c14f2c4f4ace10cc145f57c5f849c37b4bf
Samuel Prado <darkms@gmail.com>
2016-10-24 23:45:48 -0200

    Create Input class with spec file

---
+++ app/input.rb
@@ -0,0 +1,62 @@
+class Input
+  SUPPORTED_COMMANDS = %w(I C L V H S ? X).freeze
+
+  UnsupportedCommand = Class.new(StandardError)
+  InvalidInput = Class.new(StandardError)
+
+  attr_accessor :command, :row, :col, :colour
+
+  def initialize(input_string)
+    @input_string = input_string
+    @command = nil
+    @row = nil
+    @col = nil
+    @colour = nil
+  end
+
+  def parse
+    input_parameters = @input_string.split(' ')
+    raise InvalidInput if input_parameters.empty?
+
+    @command = input_parameters.shift
+    raise UnsupportedCommand, "unrecognised command :(" unless SUPPORTED_COMMANDS.include?( @command )
+
+    case @command
+      when "I"
+        check_image_creation_parameters(input_parameters)
+        @col = input_parameters[0].to_i
+        @row = input_parameters[1].to_i
+      when "L"
+        check_colour_set_parameters(input_parameters)
+        @col = input_parameters[0].to_i
+        @row = input_parameters[1].to_i
+        @colour = input_parameters[2].to_s
+      when "V"
+        check_range_parameters(input_parameters)
+        @col = input_parameters[0].to_i
+        @row = input_parameters[1].to_i..input_parameters[2].to_i
+        @colour = input_parameters[3].to_s
+      when "H"
+        check_range_parameters(input_parameters)
+        @col = input_parameters[0].to_i..input_parameters[1].to_i
+        @row = input_parameters[2].to_i
+        @colour = input_parameters[3].to_s
+      else
+        raise InvalidInput if input_parameters.any?
+    end
+  end
+
+  private
+  
+  def check_image_creation_parameters(input_parameters)
+    raise InvalidInput if input_parameters[0].nil? || input_parameters[1].nil? || input_parameters.size > 2
+  end
+
+  def check_colour_set_parameters(input_parameters)
+    raise InvalidInput if input_parameters[0].nil? || input_parameters[1].nil? || input_parameters[2].nil? || input_parameters.size > 3
+  end
+
+  def check_range_parameters(input_parameters)
+    raise InvalidInput if input_parameters[0].nil? || input_parameters[1].nil? || input_parameters[2].nil? || input_parameters[3].nil? || input_parameters.size > 4
+  end
+end
---
+++ spec/input_spec.rb
@@ -0,0 +1,236 @@
+require 'spec_helper.rb'
+
+describe Input do
+  let(:input){ Input.new(input_string) }
+
+  describe "#parse" do
+    context "invalid command" do
+      let(:input_string){ "D 3 4" }
+      it "should raise error" do
+        expect{ input.parse }.to raise_error(Input::UnsupportedCommand)
+      end
+    end
+
+    context "valid commands" do
+      context "'I' command" do
+        context "valid input" do
+          let(:input_string){ "I 5 6" }
+
+          it "should set command, row and col variables" do
+            input.parse
+            expect(input.command).to eq('I')
+            expect(input.col).to eq(5)
+            expect(input.row).to eq(6)
+          end
+        end
+
+        context "invalid input" do
+          context "missing values" do
+            let(:input_string){ "I  6" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+
+          context "more values than expected" do
+            let(:input_string){ "I 10 6 33" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'C' command" do
+        context "valid input" do
+          let(:input_string){ "C" }
+
+          it "should set command, row and col variables" do
+            input.parse
+            expect(input.command).to eq('C')
+            expect(input.col).to be_nil
+            expect(input.row).to be_nil
+          end
+        end
+
+        context "invalid input" do
+          context "more values than expected" do
+            let(:input_string){ "C 10" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'L' command" do
+        context "valid input" do
+          let(:input_string){ "L 5 6 A" }
+
+          it "should set command, row, col and colour variables" do
+            input.parse
+            expect(input.command).to eq('L')
+            expect(input.col).to eq(5)
+            expect(input.row).to eq(6)
+            expect(input.colour).to eq("A")
+          end
+        end
+
+        context "invalid input" do
+          context "missing values" do
+            let(:input_string){ "L 6" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+
+          context "more values than expected" do
+            let(:input_string){ "L 10 6 33 B" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'V' command" do
+        context "valid input" do
+          let(:input_string){ "V 10 4 6 A" }
+
+          it "should set command, row, col and colour variables" do
+            input.parse
+            expect(input.command).to eq('V')
+            expect(input.col).to eq(10)
+            expect(input.row).to eq(4..6)
+            expect(input.colour).to eq("A")
+          end
+        end
+
+        context "invalid input" do
+          context "missing values" do
+            let(:input_string){ "V 6" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+
+          context "more values than expected" do
+            let(:input_string){ "V 10 4 6 A 33" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'H' command" do
+        context "valid input" do
+          let(:input_string){ "H 10 4 6 A" }
+
+          it "should set command, row, col and colour variables" do
+            input.parse
+            expect(input.command).to eq('H')
+            expect(input.col).to eq(10..4)
+            expect(input.row).to eq(6)
+            expect(input.colour).to eq("A")
+          end
+        end
+
+        context "invalid input" do
+          context "missing values" do
+            let(:input_string){ "H 6" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+
+          context "more values than expected" do
+            let(:input_string){ "H 10 4 6 A 33" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'S' command" do
+        context "valid input" do
+          let(:input_string){ "S" }
+
+          it "should set command variables" do
+            input.parse
+            expect(input.command).to eq('S')
+            expect(input.col).to be_nil
+            expect(input.row).to be_nil
+          end
+        end
+
+        context "invalid input" do
+          context "more values than expected" do
+            let(:input_string){ "S 10 Z" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'?' command" do
+        context "valid input" do
+          let(:input_string){ "?" }
+
+          it "should set command variables" do
+            input.parse
+            expect(input.command).to eq('?')
+            expect(input.col).to be_nil
+            expect(input.row).to be_nil
+          end
+        end
+
+        context "invalid input" do
+          context "more values than expected" do
+            let(:input_string){ "? 10 Z" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+      context "'X' command" do
+        context "valid input" do
+          let(:input_string){ "X" }
+
+          it "should set command variables" do
+            input.parse
+            expect(input.command).to eq('X')
+            expect(input.col).to be_nil
+            expect(input.row).to be_nil
+          end
+        end
+
+        context "invalid input" do
+          context "more values than expected" do
+            let(:input_string){ "X 1 BA" }
+
+            it "should raise error" do
+              expect{ input.parse }.to raise_error(Input::InvalidInput)
+            end
+          end
+        end
+      end
+
+    end
+  end
+end
